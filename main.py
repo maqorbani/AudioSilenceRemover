c6eb6d9c667bf3880a9375958939fbf2a6fcb37e
Mohammad Ali Qorbani <qorbani.maq@gmail.com>
2022-08-06 13:55:45 +0430

    this file was made a cpu implementation only

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import sys
 import numpy as np
 from scipy.signal import find_peaks, peak_widths
-import torch
-import torch.nn.functional as F
 from pydub import AudioSegment
-# from pydub.playback import play
 import plotly.graph_objects as go
 
 
@@ -37,18 +34,6 @@ def sil_det(npArr, npArr2, length, frame_rate, threshold):
     npArr = npArr[a != 0].reshape(-1, 1)
     npArr2 = npArr2[a != 0].reshape(-1, 1)
     return np.concatenate((npArr, npArr2), axis=1), a
-
-
-def sil_det_gpu(npArr, npArr2, length, frame_rate, threshold):
-    frame_len = int(length * frame_rate / 2)
-    npArr = torch.tensor(npArr)
-    npArr2 = torch.tensor(npArr2)
-    thr = torch.where(npArr > threshold, 1, 0).to('cuda', type=torch.float32)
-    kernel = torch.ones(1, 1, frame_len).to('cuda')
-    thr = F.conv1d(thr.view(1, 1, -1), kernel, padding='same').cpu().view(-1)
-    npArr = (npArr[thr != 0]).cpu().view(-1, 1)
-    npArr2 = (npArr2[thr != 0]).cpu().view(-1, 1)
-    return (npArr[thr != 0]).cpu()
 
 
 def sqnc_indx(convArr):
